8f96023cc9cd3deb277bca627b5f2cfabbf7e497
İpek Çelik <iqekcelik@gmail.com>
2026-04-28 22:00:24 +0300

    test rendering and vsync

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,7 @@
 #include "Game.h"
 #include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
 Game::Game() {
@@ -29,6 +31,7 @@ void Game::Initialize(){
 	}
 
 	renderer = SDL_CreateRenderer(window, NULL );
+	SDL_SetRenderVSync(renderer, 1);
 
 	if (!renderer){
 		std::cerr << "error creating sdl renderer: " << SDL_GetError() << std::endl;
@@ -56,21 +59,41 @@ void Game::ProcessInput(){
 	}
 }
 
+glm::vec2 playerPos;
+glm::vec2 playerVelocity;
+
+void Game::Setup() {
+	playerPos = glm::vec2(10.0,10.0);
+	playerVelocity = glm::vec2(1.0, 0.0);
+}
+
 void Game::Update(){
-	// update game objects ...	
+    // double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0;
+    // millisecsPreviousFrame = SDL_GetTicks();
+
+	playerPos.x += playerVelocity.x;
+	playerPos.y += playerVelocity.y;
 }
 
 void Game::Render() {
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
 	SDL_RenderClear(renderer);
 
-	// render all game objects ...
+	// draw png texture
+	SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);	
+	SDL_DestroySurface(surface);
+	SDL_FRect dstRect = {playerPos.x, playerPos.y, 32, 32};
+
+	SDL_RenderTexture(renderer, texture, NULL, &dstRect);
+	SDL_DestroyTexture(texture);
 
 	SDL_RenderPresent(renderer);
 }
 
 void Game::Run(){
-	 while(isRunning){
+	Setup();
+	while(isRunning) {
 		ProcessInput();
 		Update();
 		Render();
--- src/Game.h
+++ src/Game.h
@@ -2,9 +2,13 @@
 
 #include <SDL3/SDL.h>
 
+const int FPS = 60;
+const int MILLISECS_PER_FRAME = 1000 /FPS;
+
 class Game {
 	
 	private:
+		// int millisecsPreviousFrame = 0;
 		bool isRunning;
 		SDL_Window* window;
 		SDL_Renderer* renderer;
@@ -13,6 +17,7 @@ class Game {
 		~Game();
 		void Initialize();
 		void Run();
+		void Setup();
 		void ProcessInput();
 		void Update();
 		void Render();
